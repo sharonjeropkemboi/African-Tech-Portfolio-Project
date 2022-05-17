2e10cc6e13f376a7ab0130db27bbd6ebfcae280e
sharon kemboi <sharonjeropkemboi@gmail.com>
2022-05-17 18:21:06 +0300

    Add event listeners

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    input("Enter your email for my weekly newsletter");
+	alert("Hello!");
+	alert("How are you?");
+    alert("Welcome to Sharonne Kemboi Personal Portfolio");
+    
+}    
+
+function changeText() {
+	var span = document.getElementById("output");
+	var textBox = document.getElementById("textbox");
+	
+	 textbox.style.color = "red"; 
+		
+}
+
+function changeImage() {
+	var stellaimg = document.getElementById("sharonimg");
+	
+	 stellaimg.style.float = "left"; 
+		
+}
